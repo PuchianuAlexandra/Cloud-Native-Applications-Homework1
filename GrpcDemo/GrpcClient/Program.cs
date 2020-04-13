764c7145f540be4272d4b455c7d0336db95fce48
PuchianuAlexandra <62277662+PuchianuAlexandra@users.noreply.github.com>
2020-04-14 00:12:10 +0300

    first commit

---
+++ GrpcDemo/GrpcClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+           // var input = new HelloRequest { Name = "Tim" };
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+
+          //  var reply = await client.SayHelloAsync(input);
+
+           // Console.WriteLine(reply.Message);
+           
+            Console.WriteLine("Tape a name: ");
+
+            //Console.ReadLine();
+           
+            string name = Console.ReadLine();
+          
+            var response = await client.SayHelloAsync(new HelloRequest { Name = name });
+            Console.WriteLine("\n" + "Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
